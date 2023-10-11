6d686344094f5e71a3a3b0c34fb46f3f794da225
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-10-11 18:44:08 +0200

    more comments on cofig example

--- js/config.example.js
+++ js/config.example.js
@@ -1,11 +1,11 @@
 const config = {
     "mapbox": {
-        "accessToken": "pk.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "style": "mapbox://styles/mapbox/streets-v12",
-        "containerId": "map"
+        "accessToken": "pk.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // get one from https://account.mapbox.com/
+        "style": "mapbox://styles/mapbox/streets-v12",               // this is the default public style, but you can create your own on https://studio.mapbox.com/
+        "containerId": "map"                                         // container id in index.html
     },
     "image": {
-        "url": "img/Weedy-1.png", //chrome.exe --allow-file-access-from-files
+        "url": "img/Weedy-1.png",   //picture from your property (this picture is the reason you have to start your browser like this chrome.exe --allow-file-access-from-files
         "coordinates": [
             [3.438457, 50.868191],  //top left
             [3.439126, 50.868411],  //top right
@@ -14,6 +14,6 @@ const config = {
         ]
     },
     "willow": {
-        "host": "http://192.168.1.2:8080"
+        "host": "http://192.168.1.2:8080"   //host of your willow
     }
 }
